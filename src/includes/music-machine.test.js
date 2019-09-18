f14ec1834f3f6721ed2e47c7e5792b5bfe1bfda6
John Hodgson <johnhodgson@johnsmacbook.lan>
2019-09-18 23:03:20 +0100

    Added more tests

--- src/includes/music-machine.test.js
+++ src/includes/music-machine.test.js
@@ -3,25 +3,40 @@ import samples from '../data/samples';
 import * as audioContext from './audio-context';
 import * as metronome from './metronome/index';
 import * as samplesBuffer from './samples-buffer';
+import { default as song } from '../songs/VanHalenJump';
 import {
   setUpDependencies,
   configureMetronome,
   onMetronomeMessage,
   doTick,
+  triggerSounds,
+  calculateNextBeatTime,
+  incrementBeat,
+  rewire$getState,
+  rewire$dispatch,
   rewire$doTick,
-  rewire$getState
+  rewire$triggerSounds,
+  rewire$calculateNextBeatTime,
+  rewire$incrementBeat
 } from './music-machine';
 
 const musicMachine = {
-  doTick
+  doTick,
+  triggerSounds,
+  calculateNextBeatTime,
+  incrementBeat
 };
 
 function resetRewiredFunctions() {
   rewire$doTick(musicMachine.doTick);
+  rewire$triggerSounds(musicMachine.triggerSounds);
+  rewire$calculateNextBeatTime(musicMachine.calculateNextBeatTime);
+  rewire$incrementBeat(musicMachine.incrementBeat);
 }
 
 afterEach(() => {
   resetRewiredFunctions();
+  mockDispatch.mockReset();
 });
 
 audioContext.Context = {
@@ -58,6 +73,9 @@ const mockState = {
 };
 
 rewire$getState(() => mockState);
+
+const mockDispatch = jest.fn();
+rewire$dispatch(mockDispatch);
 
 describe('setUpDependencies', () => {
   it('should return audio context, metronome and samplesBuffer', async () => {
@@ -96,7 +114,7 @@ describe('configureMetronome', () => {
 });
 
 describe('onMetronomeMessage', () => {
-  it('on tick event should call doTick with correct params', async () => {
+  it('on tick event should call doTick with correct params', () => {
     const spy = jest.fn();
     rewire$doTick((songData, tempo, nextBeatTime, beatNumber) =>
       spy(songData, tempo, nextBeatTime, beatNumber)
@@ -108,5 +126,87 @@ describe('onMetronomeMessage', () => {
     expect(spy.mock.calls[0][1]).toEqual(120);
     expect(spy.mock.calls[0][2]).toEqual(20);
     expect(spy.mock.calls[0][3]).toEqual(15);
+  });
+});
+
+describe('onMetronomeMessage', () => {
+  it('on tick event should call doTick with correct params', () => {
+    const spy = jest.fn();
+    rewire$doTick((arg1, arg2, arg3, arg4) => spy(arg1, arg2, arg3, arg4));
+
+    onMetronomeMessage({ songData: 'mock!' })({ data: 'tick' });
+
+    expect(spy.mock.calls[0][0]).toEqual({ songData: 'mock!' });
+    expect(spy.mock.calls[0][1]).toEqual(120);
+    expect(spy.mock.calls[0][2]).toEqual(20);
+    expect(spy.mock.calls[0][3]).toEqual(15);
+  });
+});
+
+describe('doTick', () => {
+  it('should call triggerSounds with correct params if next beat is due', async () => {
+    const spy = jest.fn();
+    rewire$triggerSounds((arg1, arg2, arg3, arg4) =>
+      spy(arg1, arg2, arg3, arg4)
+    );
+
+    doTick({ songData: 'mock!' }, 120, 20, 15);
+
+    expect(spy.mock.calls[0][0]).toEqual({ songData: 'mock!' });
+    expect(spy.mock.calls[0][1]).toEqual(20);
+    expect(spy.mock.calls[0][2]).toEqual(120);
+    expect(spy.mock.calls[0][3]).toEqual(15);
+  });
+
+  it("shouldn't call triggerSounds if next next beat is not due", async () => {
+    const spy = jest.fn();
+    rewire$triggerSounds(param => {
+      spy(param);
+    });
+
+    doTick(song, 120, 105, 15);
+
+    expect(spy.mock.calls.length).toBe(0);
+  });
+
+  it('should call calculateNextBeatTime with correct params if next beat is due', async () => {
+    const spy = jest.fn();
+    rewire$calculateNextBeatTime((arg1, arg2) => spy(arg1, arg2));
+
+    doTick(song, 120, 20, 15);
+
+    expect(spy.mock.calls[0][0]).toEqual(120);
+    expect(spy.mock.calls[0][1]).toEqual(20);
+  });
+
+  it('should call incrementBeat with correct params if next beat is due', async () => {
+    const spy = jest.fn();
+    rewire$incrementBeat(arg1 => spy(arg1));
+
+    doTick(song, 120, 20, 15);
+
+    expect(spy.mock.calls[0][0]).toEqual(15);
+  });
+});
+
+describe('calculateNextBeatTime', () => {
+  it('should dispatch a SET_NEXT_BEAT_TIME action', async () => {
+    calculateNextBeatTime(120, 20);
+
+    expect(mockDispatch.mock.calls[0][0]).toEqual({
+      type: 'SET_NEXT_BEAT_TIME',
+      value: 20.125
+    });
+  });
+});
+
+describe('incrementBeat', () => {
+  it('should dispatch a SET_BEAT_NUMBER action', async () => {
+    incrementBeat(20);
+
+    expect(mockDispatch.mock.calls[0][0]).toEqual({
+      type: 'SET_BEAT_NUMBER',
+      value: 21
+    });
   });
 });
