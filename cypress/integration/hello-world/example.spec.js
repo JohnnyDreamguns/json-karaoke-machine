d552df5b084eded269814c6c32443f0e34c0cc44
John Hodgson <mrjohnhodgson@gmail.com>
2021-10-31 21:28:46 +0000

    sdvsdvds

--- cypress/integration/hello-world/example.spec.js
+++ cypress/integration/hello-world/example.spec.js
@@ -12,27 +12,7 @@
 // https://on.cypress.io/introduction-to-cypress
 
 describe('json karaoke machine', () => {
-  beforeEach(() => {
-    // Cypress starts out with a blank slate for each test
-    // so we must tell it to visit our website with the `cy.visit()` command.
-    // Since we want to visit the same URL at the start of all our tests,
-    // we include it in our beforeEach function so that it runs before each test
-    cy.visit('http://localhost:1234');
+  it('TODO', () => {
+    expect(true).to.equal(true);
   });
-
-  it(
-    'TODO',
-    {
-      retries: {
-        runMode: 0,
-        openMode: 0,
-      },
-    },
-    () => {
-      cy.get('h1').should(
-        'have.text',
-        'Press play to hear the sweet sounds of the Web Audio API'
-      );
-    }
-  );
 });
